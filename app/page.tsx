278f00e5cf4bab8b9c735cba203850ec0bcb1663
Matt Ballard <mattjballard81@gmail.com>
2024-07-10 17:10:29 +0800

    add basic components and structure navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1 className='text-3xl'>Home Page</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>
+        click me!
+      </Button>
     </main>
   );
 }
