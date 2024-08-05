01dc81d5449f023a5849104a8579e915414323cd
Matt Ballard <mattjballard81@gmail.com>
2024-08-05 15:24:30 -0400

    add functionality to update basic user info

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,42 @@
+import { fetchProfile, updateProfileAction } from "@/utils/actions";
 import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
 
-function ProfilePage() {
+async function ProfilePage() {
+  const profile = await fetchProfile();
+  console.log(profile);
+
   return (
-    <div>
-      <h1 className='text-3xl'>Profile Page</h1>
-    </div>
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>User Profile</h1>
+      <div className='border p-8 rounded-md '>
+        {/* image */}
+        <FormContainer action={updateProfileAction}>
+          <div className='grid md:grid-cols-2 gap-4 mt-4'>
+            <FormInput
+              name='firstName'
+              type='text'
+              label='First Name'
+              defaultValue={profile.firstName}
+            />
+            <FormInput
+              name='lastName'
+              type='text'
+              label='Last Name'
+              defaultValue={profile.lastName}
+            />
+            <FormInput
+              name='username'
+              type='text'
+              label='Username'
+              defaultValue={profile.username}
+            />
+          </div>
+          <SubmitButton text='Update Profile' className='mt-8' />
+        </FormContainer>
+      </div>
+    </section>
   );
 }
 
--- utils/actions.ts
+++ utils/actions.ts
@@ -6,12 +6,28 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route.");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred.",
+  };
+};
+
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
     const user = await currentUser();
-    console.log(user);
+    // console.log(user);
     if (!user) {
-      throw new Error("No user found. Please log in or sign up");
+      throw new Error("Please log in or sign up to create a profile.");
     }
 
     const rawData = Object.fromEntries(formData);
@@ -31,10 +47,7 @@ export const createProfileAction = async (prevState: any, formData: FormData) =>
       },
     });
   } catch (error) {
-    // console.log(error);
-    return {
-      message: error instanceof Error ? error.message : "An error occurred.",
-    };
+    return renderError(error);
   }
   redirect("/");
 };
@@ -53,4 +66,36 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: { clerkId: user.id },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath("/profile");
+    return { message: "Profile successfully updated." };
+  } catch (error) {
+    return renderError(error);
+  }
 };
