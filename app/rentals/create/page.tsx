316039c0a0239ab4c4f6c66b08a236fed9c2b743
Matt Ballard <mattjballard81@gmail.com>
2024-09-09 12:49:52 -0400

    add accommodation details inputs to create property page

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,7 @@ import CategoriesInput from "@/components/form/CategoriesInput";
 import TextAreaInput from "@/components/form/TextAreaInput";
 import CountriesInput from "@/components/form/CountriesInput";
 import ImageInput from "@/components/form/ImageInput";
+import CounterInput from "@/components/form/CounterInput";
 
 function CreatePropertyPage() {
   return (
@@ -39,6 +40,11 @@ function CreatePropertyPage() {
             <CountriesInput />
             <ImageInput />
           </div>
+          <h3 className='text-lg mt-8 mb-4 font-medium'>Accommodation Details</h3>
+          <CounterInput detail='guests' />
+          <CounterInput detail='bedrooms' />
+          <CounterInput detail='beds' />
+          <CounterInput detail='bathrooms' />
           <SubmitButton text='Create Rental' className='mt-12' />
         </FormContainer>
       </div>
