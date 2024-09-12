3bb817de00a5ff6b147084bb72f745c4d811b7ee
Matt Ballard <mattjballard81@gmail.com>
2024-09-11 20:53:09 -0400

    add remaining components to property card with temporary hardcoded values

--- components/card/CountryAndFlag.tsx
+++ components/card/CountryAndFlag.tsx
@@ -1,4 +1,16 @@
-function CountryAndFlag() {
-  return <div>CountryAndFlag</div>;
+import { findCountryByCode } from "@/utils/countries";
+
+function CountryAndFlag({ countryCode }: { countryCode: string }) {
+  const validCountry = findCountryByCode(countryCode);
+  const countryName =
+    validCountry?.name.length > 20
+      ? `${validCountry?.name.subString(0, 20)}...`
+      : validCountry.name;
+
+  return (
+    <span className='flex justify-between items-center gap-2 text-sm'>
+      {validCountry.flag} {countryName}
+    </span>
+  );
 }
 export default CountryAndFlag;
--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import CountryFlagAndName from "./CountryFlagAndName";
 import PropertyRating from "./PropertyRating";
 import FavoriteToggleButton from "./FavoriteToggleButton";
 import { PropertyCardProps } from "@/utils/types";
 import { formatCurrency } from "@/utils/format";
+import CountryAndFlag from "./CountryAndFlag";
 
 function PropertyCard({ property }: { property: PropertyCardProps }) {
   const { name, image, price, country, id: propertyId, tagline } = property;
@@ -23,19 +23,22 @@ function PropertyCard({ property }: { property: PropertyCardProps }) {
         <div className='flex justify-between items-center'>
           <h3 className='text-sm font-semibold mt-1'>{name.substring(0, 30)}</h3>
           {/* property rating */}
+          <PropertyRating inPage={false} propertyId={propertyId} />
         </div>
         <p className='text-sm mt-1 text-muted-foreground'>
           {tagline.substring(0, 40)}
         </p>
         <div className='flex justify-between items-center mt-1'>
           <p className='text-sm mt-1'>
-            <span className='font-semibold'>{formatCurrency(price)}</span>
+            <span className='font-semibold'>{formatCurrency(price)}/night</span>
           </p>
           {/* country and flag */}
+          <CountryAndFlag countryCode={country} />
         </div>
       </Link>
       <div className='absolute top-5 right-5 z-5'>
         {/* favorite toggle button */}
+        <FavoriteToggleButton propertyId={propertyId} />
       </div>
     </article>
   );
