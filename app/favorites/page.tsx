c111667f4c15a0884bfb14a9b1309d5d41520d2c
Matt Ballard <mattjballard81@gmail.com>
2024-09-22 21:58:14 -0400

    display property cards on favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,9 +1,14 @@
-function FavoritesPage() {
-  return (
-    <div>
-      <h1 className='text-3xl'>Favorites Page</h1>
-    </div>
-  );
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
+
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+  return <PropertiesList properties={favorites} />;
 }
 
 export default FavoritesPage;
